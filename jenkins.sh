ad75c402889c96ce8a08c26bd1ba3fcf0a53b570
dhootvinamra <dhoot.vinamra@gmail.com>
2018-12-12 15:06:16 +0530

    Modified for checking the pulled branch name in jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "[INFO] Running Job"
 echo "[INFO] file updated for POLL SCM demo after configuring jenkins with CHMOD "
+
+echo "[INFO] Modified for checking the pulled branch name in jenkins"
